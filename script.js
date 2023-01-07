2e54c2996a0f130bd4af388dc2bf08b3df3de181
kibeetfelix <felixkibeet@gmail.com>
2023-01-07 22:12:32 +0300

    inputEl

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const formEl = document.querySelector(".form");
 
+const inputEl = document.querySelector(".input")
+
 
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
-})
+    toDoList()
+});
+
+function toDoList() {
+
+}
